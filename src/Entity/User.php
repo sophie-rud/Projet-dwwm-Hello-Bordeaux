d7b8d6b3ce1610a15a328650959b09b8722d9710
sophie-rud <sophie.rudio@lapiscine.pro>
2024-09-14 11:17:46 +0200

    Security and style on user form

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,11 +7,13 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[ORM\Table(name: 'user', uniqueConstraints: [new ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])])]
+#[UniqueEntity(fields: ['email'], message: 'Cet email est déjà utilisé.')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -19,11 +19,29 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', null, [
+                'attr' => [
+                    'class' => 'custom-input',
+                    'type' => 'email',
+                ],
+                'label_attr' => ['class' => 'custom-label'],
+                'label' => 'Email*',
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'L\'email ne peut pas être vide.',
+                    ]),
+                    new Assert\Email([
+                        'message' => 'Veuillez entrer une adresse email valide.',
+                    ]),
+                ],
+            ])
             /* ->add('roles') */
             ->add('password', PasswordType::class , [
                 /*'hash_property_path' => 'password', // autre manière de hasher le mdp, nous l'avons fait dans le controller
                 'mapped' => false,*/
+                'attr' => ['class' => 'custom-input'],
+                'label_attr' => ['class' => 'custom-label'],
+                'label' => 'Mot de passe*',
                 'constraints' => [
                     new Assert\NotBlank([
                         'message' => 'Le mot de passe ne peut pas être vide.',
@@ -37,14 +55,93 @@ class UserType extends AbstractType
                         'message' => 'Le mot de passe doit contenir au moins une majuscule et un chiffre.',
                     ]),
                 ],
-                ] )
-            ->add('username')
-            ->add('firstName')
-            ->add('birthDate', null, [
-                'widget' => 'single_text'
             ])
-            ->add('phone')
-            ->add('presentation')
+            ->add('username', null, [
+                'attr' => ['class' => 'custom-input'],
+                'label_attr' => ['class' => 'custom-label'],
+                'label' => 'Pseudo*',
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Le nom d\'utilisateur ne peut pas être vide.',
+                    ]),
+                    new Assert\Length([
+                        'min' => 3,
+                        'minMessage' => 'Le nom d\'utilisateur doit contenir au moins {{ limit }} caractères.',
+                        'max' => 20,
+                        'maxMessage' => 'Le nom d\'utilisateur ne doit pas dépasser {{ limit }} caractères.',
+                    ]),
+                    new Assert\Regex([
+                        'pattern' => '/^[a-zA-Z0-9_]+$/',
+                        'message' => 'Le nom d\'utilisateur ne peut contenir que des lettres, des chiffres et des underscores.',
+                    ]),
+                ],
+            ])
+            ->add('firstName', null, [
+                'attr' => ['class' => 'custom-input'],
+                'label_attr' => ['class' => 'custom-label'],
+                'label' => 'Prénom',
+                'constraints' => [
+                    new Assert\Length([
+                        'min' => 2,
+                        'minMessage' => 'Le prénom doit contenir au moins {{ limit }} caractères.',
+                        'max' => 50,
+                        'maxMessage' => 'Le prénom ne doit pas dépasser {{ limit }} caractères.',
+                    ]),
+                    new Assert\Regex([
+                        'pattern' => '/^[a-zA-ZÀ-ÿ\- ]+$/',
+                        'message' => 'Le prénom ne peut contenir que des lettres, des espaces et des tirets.',
+                    ]),
+                ],
+            ])
+            ->add('birthDate', null, [
+                'widget' => 'single_text', //input de type "date"
+                'attr' => [
+                    'class' => 'custom-input',
+                    'min' => '1920-01-01', // définit une date maximum
+                    'max' => (new \DateTime())->format('Y-m-d'), // définit une date minimum (jour actuel)
+                ],
+                'label_attr' => ['class' => 'custom-label'],
+                'label' => 'Date de naissance*',
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'La date de naissance ne peut pas être vide.',
+                    ]),
+                    new Assert\Date([
+                        'message' => 'Veuillez entrer une date valide.',
+                    ]),
+                    new Assert\LessThan([
+                        'value' => (new \DateTime())->format('Y-m-d'),
+                        'message' => 'La date de naissance doit être dans le passé.',
+                    ]),
+                ],
+            ])
+            ->add('phone', null, [
+                'attr' => [
+                    'class' => 'custom-input',
+                    'type' => 'tel',  // type: numéro de téléphone
+                    'pattern' => '[0-9]{10}',  // Exige un format de 10 chiffres
+                    'placeholder' => '06XXXXXXXX', //Exemple
+                ],
+                'label_attr' => ['class' => 'custom-label'],
+                'label' => 'Numéro de téléphone',
+                'constraints' => [
+                    new Assert\Regex([
+                        'pattern' => '/^(\d{10})$/',
+                        'message' => 'Le numéro de téléphone doit comporter 10 chiffres.',
+                    ]),
+                ],
+            ])
+            ->add('presentation', null, [
+                'attr' => ['class' => 'custom-input'],
+                'label_attr' => ['class' => 'custom-label'],
+                'label' => 'Présentation',
+                'constraints' => [
+                        new Assert\Length([
+                        'max' => 1000, // Limite de caractères
+                        'maxMessage' => 'La présentation ne peut pas dépasser {{ limit }} caractères.',
+                    ]),
+                ],
+            ])
             /* ->add('activitiesParticipate', EntityType::class, [
                 'class' => Activity::class,
                 'choice_label' => 'id',
@@ -52,9 +149,14 @@ class UserType extends AbstractType
             ]) */
             ->add('profilePicture', FileType::class, [
             'mapped' => false,
+            'attr' => ['class' => 'custom-input'],
+            'label_attr' => ['class' => 'custom-label'],
+            'label' => 'Choisir une image de profil*'
             ])
-            ->add('enregistrer', SubmitType::class)
-        ;
+            ->add('enregistrer', SubmitType::class,[
+                'attr' => ['class' => 'submit-btn'],
+                'label' => 'Valider'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
